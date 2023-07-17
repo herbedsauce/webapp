a82db9db069332896b8777e56aac6b74c2fcb2cd
graan501 <danielle.vangraan@kpn.com>
2023-07-17 20:31:52 +0200

    add firebase script

--- herbedsauce/src/components/app-root/app-root.tsx
+++ herbedsauce/src/components/app-root/app-root.tsx
@@ -1,11 +1,31 @@
 import { Component, h } from '@stencil/core';
-
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 @Component({
   tag: 'app-root',
   styleUrl: 'app-root.css',
   shadow: true,
 })
 export class AppRoot {
+  componentWillLoad() {
+    // Your web app's Firebase configuration
+    // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+    const firebaseConfig = {
+      apiKey: 'AIzaSyCaMUNdBLEjWR6uBg2anDBwspLNpV1Rh9M',
+      authDomain: 'herbedsauce-cd6f6.firebaseapp.com',
+      projectId: 'herbedsauce-cd6f6',
+      storageBucket: 'herbedsauce-cd6f6.appspot.com',
+      messagingSenderId: '526668315816',
+      appId: '1:526668315816:web:e804070b7346911948d69d',
+      measurementId: 'G-2LYQZLZJYK',
+    };
+
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+    const analytics = getAnalytics(app);
+  }
   render() {
     return (
       <div>
